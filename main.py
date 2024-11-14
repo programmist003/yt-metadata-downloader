55132dc593eb513e2835b05f719b2b113eb9483c
programmist <programmist.030@gmail.com>
2024-11-15 00:26:19 +0400

    some code written

---
+++ main.py
@@ -0,0 +1,21 @@
+from googleapiclient.discovery import build
+import toml
+from icecream import ic
+from urllib.request import urlopen
+
+
+DEVELOPER_KEY = toml.load("config.toml")["api_key"]
+# API client
+youtube = build("youtube", "v3", developerKey=DEVELOPER_KEY)
+# Notice that nextPageToken now is requested in 'fields' parameter
+request = youtube.search().list(
+    part="id,snippet",
+    type="video",
+    q="Spider-Man",
+    videoDuration="short",
+    videoDefinition="high",
+    maxResults=1,
+    fields="nextPageToken,items(id(videoId),snippet(publishedAt,channelId,channelTitle,title,description))",
+)
+response = request.execute()
+ic(response)

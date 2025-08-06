0db2fc82579949883a6c6ffbcd155e46330f9a5d
programmist <programmist.030@gmail.com>
2025-08-06 18:24:09 +0400

    resource_id_getter.py: property created

---
+++ properties/resource_id_getter.py
@@ -0,0 +1,16 @@
+"""Resource ID getter module"""
+
+from typing import NamedTuple
+from typing import Callable
+
+ResourceIdGetterFunction = Callable[[str], str | None]
+
+
+class ResourceIdGetter(NamedTuple):
+    """Resource ID getter"""
+
+    get_resource_id: ResourceIdGetterFunction
+
+    def __call__(self, url: str) -> str | None:
+        """Get resource ID from a YouTube URL"""
+        return self.get_resource_id(url)

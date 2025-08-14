de5e1655721cca457d9b98b28b6fa9ead1163aaa
programmist <programmist.030@gmail.com>
2025-08-15 00:45:47 +0400

    video.py: save_videos_data + DataGettter -> Video

--- kinds/video.py
+++ kinds/video.py
@@ -3,6 +3,7 @@
 import json
 from furl import furl
 from kinds.kind import Kind
+from properties.data_getter import DataGetter
 from properties.resource_id_getter import ResourceIdGetter
 from urlparse import check_domain
 from auth import youtube
@@ -13,6 +14,7 @@ class Video(Kind):
 
     def __init__(self, *properties: list):
         self.add(ResourceIdGetter(get_video_id))
+        self.add(DataGetter(save_videos_data))
         super().__init__(*properties)
 
 
@@ -35,3 +37,17 @@ def save_video_data(video_id):
     response = request.execute()
     with open(f"{video_id}.json", "w", encoding="utf-8") as f:
         json.dump(response, f, ensure_ascii=False, indent=4)
+
+def save_videos_data(video_ids: list[str]):
+    """Save video data to a JSON file"""
+    request = youtube.videos().list(  # pylint: disable=no-member
+        part="contentDetails, id, liveStreamingDetails, "
+        "localizations, paidProductPlacementDetails, player, "
+        "recordingDetails, snippet, statistics, status, topicDetails",
+        id=video_ids,
+    )
+    videos_data = request.execute().items
+    for video_data in videos_data:
+        video_id = video_data["id"]
+        with open(f"{video_id}.json", "w", encoding="utf-8") as f:
+            json.dump(video_data, f, ensure_ascii=False, indent=4)

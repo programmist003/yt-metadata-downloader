b6de599b868aa22d1fcfe8f425349b7143143c76
programmist <programmist.030@gmail.com>
2025-08-17 02:58:52 +0400

    video.py: save_video_data fixed and upgraded; toolz library installed

--- kinds/video.py
+++ kinds/video.py
@@ -1,6 +1,8 @@
 """YouTube video resource kind module"""
 
 import json
+from icecream import ic
+from toolz import partition_all
 from furl import furl
 from kinds.kind import Kind
 from properties.data_getter import DataGetter
@@ -40,14 +42,19 @@ def save_video_data(video_id):
 
 def save_videos_data(video_ids: list[str]):
     """Save video data to a JSON file"""
-    request = youtube.videos().list(  # pylint: disable=no-member
-        part="contentDetails, id, liveStreamingDetails, "
-        "localizations, paidProductPlacementDetails, player, "
-        "recordingDetails, snippet, statistics, status, topicDetails",
-        id=video_ids,
-    )
-    videos_data = request.execute().items
-    for video_data in videos_data:
-        video_id = video_data["id"]
-        with open(f"{video_id}.json", "w", encoding="utf-8") as f:
-            json.dump(video_data, f, ensure_ascii=False, indent=4)
+    PARTITION_LENGTH = 50 # pylint: disable=invalid-name
+    partitions = list(partition_all(PARTITION_LENGTH, video_ids))
+    for partition in partitions:
+        request = youtube.videos().list(  # pylint: disable=no-member
+            part="contentDetails, id, liveStreamingDetails, "
+            "localizations, paidProductPlacementDetails, player, "
+            "recordingDetails, snippet, statistics, status, topicDetails",
+            id=ic(list(partition)),
+            maxResults=PARTITION_LENGTH,
+        )
+        videos_data = request.execute().get("items", [])
+
+        for video_data in videos_data:
+            video_id = video_data["id"]
+            with open(f"video[{video_id}].json", "w", encoding="utf-8") as f:
+                json.dump(video_data, f, ensure_ascii=False, indent=4)

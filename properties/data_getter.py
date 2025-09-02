0fd478143bcabe60be0c28b7d3bf6a02e371fd2b
programmist <programmist.030@gmail.com>
2025-09-02 22:47:24 +0400

    data_getter.py: default value of DataGetter

--- properties/data_getter.py
+++ properties/data_getter.py
@@ -8,7 +8,7 @@ DataGetterFunction = Callable[[list[str]], list[dict]]
 class DataGetter(NamedTuple):
     """Data getter"""
 
-    data_getter: DataGetterFunction
+    data_getter: DataGetterFunction = lambda x: list()
 
     def __call__(self, resource_id: list[str]) -> list[dict]:
         return self.data_getter(resource_id)

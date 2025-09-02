30650e5fb64176b73155873a39775552eda5b3df
programmist <programmist.030@gmail.com>
2025-09-02 16:30:54 +0400

    ResourceIdGetter has default value

--- properties/resource_id_getter.py
+++ properties/resource_id_getter.py
@@ -9,7 +9,7 @@ ResourceIdGetterFunction = Callable[[str], str | None]
 class ResourceIdGetter(NamedTuple):
     """Resource ID getter"""
 
-    get_resource_id: ResourceIdGetterFunction
+    get_resource_id: ResourceIdGetterFunction = lambda x: None
 
     def __call__(self, url: str) -> str | None:
         """Get resource ID from a YouTube URL"""
